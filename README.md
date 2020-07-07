e73c9023daa0479a607b3cc2225bc6bd20d8d8c8
agjs <hi@aleksandar.xyz>
2020-07-07 12:00:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-7eendc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-7eendc)
---
+++ index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
