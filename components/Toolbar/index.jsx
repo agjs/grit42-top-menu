99979722538b09c11aaaecda7a289540b11c0494
agjs <hi@aleksandar.xyz>
2020-07-07 14:07:25 +0200

    3 files modified

--- components/Toolbar/index.jsx
+++ components/Toolbar/index.jsx
@@ -1,13 +1,34 @@
 import React from "react";
+import className from "classnames";
 import "./style.css";
 
 const Toolbar = props => {
-  return <nav className="grit42-toolbar">{props.children}</nav>;
+  return <nav className={className("grit42-toolbar")}>{props.children}</nav>;
+};
+
+const ToolbarBlockGroup = props => {
+  const children = Array.isArray(props.children)
+    ? props.children.map((group, index) => {
+        return React.cloneElement(
+          group,
+          Object.assign({}, group.props, {
+            hasBorder: index !== props.children.length - 1
+          })
+        );
+      })
+    : props.children;
+  return (
+    <div className={className("grit42-toolbar__block-group")}>{children}</div>
+  );
 };
 
 const ToolbarBlock = props => {
   return (
-    <div className="grit42-toolbar__block">
+    <div
+      className={className("grit42-toolbar__block", {
+        "grit42-toolbar__block--with-border": props.hasBorder
+      })}
+    >
       <div className="grit42-toolbar__block__content">
         <span className="grit42-toolbar__block__title">{props.title}</span>
         {props.children}
@@ -23,20 +44,35 @@ const ToolbarIcons = props => {
 export default props => {
   return (
     <Toolbar>
-      <ToolbarBlock title="Views">
-        <ToolbarIcons>
-          <span>Icon 1</span>
-          <span>Icon 2</span>
-          <span>Icon 3</span>
-        </ToolbarIcons>
-      </ToolbarBlock>
-      <ToolbarBlock title="Ex- & Import">
-        <ToolbarIcons>
-          <span>Icon 1</span>
-          <span>Icon 2</span>
-          <span>Icon 3</span>
-        </ToolbarIcons>
-      </ToolbarBlock>
+      <ToolbarBlockGroup>
+        <ToolbarBlock title="Views">
+          <ToolbarIcons>
+            <span className="grit42-toolbar__block__icons__icon">Icon 1</span>
+            <span className="grit42-toolbar__block__icons__icon">Icon 2</span>
+            <span className="grit42-toolbar__block__icons__icon">Icon 3</span>
+          </ToolbarIcons>
+        </ToolbarBlock>
+      </ToolbarBlockGroup>
+      <ToolbarBlockGroup>
+        <ToolbarBlock title="Compounds">
+          <ToolbarIcons>
+            <span className="grit42-toolbar__block__icons__icon">Icon 1</span>
+            <span className="grit42-toolbar__block__icons__icon">Icon 2</span>
+            <span className="grit42-toolbar__block__icons__icon">Icon 3</span>
+          </ToolbarIcons>
+          <ToolbarIcons>
+            <span className="grit42-toolbar__block__icons__icon">Icon 1</span>
+            <span className="grit42-toolbar__block__icons__icon">Icon 2</span>
+            <span className="grit42-toolbar__block__icons__icon">Icon 3</span>
+          </ToolbarIcons>
+        </ToolbarBlock>
+        <ToolbarBlock title="Ex- & Import">
+          <ToolbarIcons>
+            <span className="grit42-toolbar__block__icons__icon">Icon 1</span>
+            <span className="grit42-toolbar__block__icons__icon">Icon 2</span>
+          </ToolbarIcons>
+        </ToolbarBlock>
+      </ToolbarBlockGroup>
     </Toolbar>
   );
 };
